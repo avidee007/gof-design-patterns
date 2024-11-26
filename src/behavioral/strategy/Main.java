f0a93362f25f28801841ef89b1c4331ca962501b
Avinash Kumar <kumaravinash2@contractor.bfusa.com>
2024-11-26 19:18:55 +0530

    Strategy design pattern example client code fixed

--- src/behavioral/strategy/Main.java
+++ src/behavioral/strategy/Main.java
@@ -5,17 +5,17 @@ public class Main {
   public static void main(String[] args) {
 
     // User selects credit payment
-    PaymentStrategy creditCardPayment = new CreditCardPayment("123456789999", "Test", 123, 9, 2027);
-    creditCardPayment.pay(100.00);
+    PaymentContext paymentContext =
+        new PaymentContext(new CreditCardPayment("123456789999", "Test", 123, 9, 2027));
+    paymentContext.executePayment(100.00);
 
     // User selects paypal payment
-    PaymentStrategy paypalPayment = new PaypalPayment("test@test.com");
-    paypalPayment.pay(100.00);
+    paymentContext = new PaymentContext(new PaypalPayment("test@test.com"));
+    paymentContext.executePayment(100.00);
 
     // User selects UPI payment
-    PaymentStrategy upiPayment = new UpiPayment("1234567890");
-    upiPayment.pay(100.00);
-
+    paymentContext = new PaymentContext(new UpiPayment("1234567890"));
+    paymentContext.executePayment(100.00);
 
   }
 }
