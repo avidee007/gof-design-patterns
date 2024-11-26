3c176ef9867acbc8fcfe2642b2a777ce7700b039
Avinash Kumar <kumaravinash2@contractor.bfusa.com>
2024-11-26 20:35:59 +0530

    Factory design pattern example client code fixed

--- src/creational/factory/Main.java
+++ src/creational/factory/Main.java
@@ -3,11 +3,11 @@ package creational.factory;
 public class Main {
   public static void main(String[] args) {
     //Client wants to send email notification
-    Notification email = NotificationFactory.notificationInstance("EMAIL");
-    email.notifyUser();
+    Notification notification = NotificationFactory.notificationInstance("EMAIL");
+    notification.notifyUser();
 
-    //Client wants to send email notification
-    Notification sms = NotificationFactory.notificationInstance("SMS");
-    sms.notifyUser();
+    //Client wants to send SMS notification
+    notification = NotificationFactory.notificationInstance("SMS");
+    notification.notifyUser();
   }
 }
